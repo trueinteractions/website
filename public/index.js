b610ff68865d28e0ac94315b76e08ad3465a4c41
brannonlucas <brannonlucas@gmail.com>
2015-06-27 23:58:45 -0600

    fustercluck

--- public/index.js
+++ public/index.js
@@ -2,5 +2,22 @@ window.addEventListener('load', function() {
 	$('#menu').on('click', function() {
 		$('.left.sidebar').sidebar('toggle');
 	});
+
 	$('.checkbox').checkbox();
+
+  $('.ui.form').form({
+    email: {
+      identifier: 'email-address',
+      rules: [
+        {
+          type: 'empty',
+          prompt: 'Please enter your email address'
+        },
+        {
+          type: 'email',
+          prompt: 'Please enter a valid email address'
+        }
+      ]
+    }
+  })
 });
